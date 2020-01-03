eac9f04ba39cde1d6ef335245398420d2ab3440b
Arkadiusz <arkadiusz.sapek@skygate.io>
2020-01-03 13:21:33 +0100

    Add chartsjs to dashboard

--- frontend/src/app/features/dashboard/components/dashboard/dashboard.component.ts
+++ frontend/src/app/features/dashboard/components/dashboard/dashboard.component.ts
@@ -12,12 +12,34 @@ export class DashboardComponent {
     options: GridsterConfig
     dashboard: Array<GridsterItem>
 
+    public barChartOptions = {
+        scaleShowVerticalLines: false,
+        responsive: true,
+        maintainAspectRatio: false
+    }
+
+    public barChartLabels = [
+        '2006',
+        '2007',
+        '2008',
+        '2009',
+        '2010',
+        '2011',
+        '2012'
+    ]
+    public barChartType = 'bar'
+    public barChartLegend = true
+    public barChartData = [
+        { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+        { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+    ]
+
     ngOnInit() {
         this.options = gridSystemOptions
 
         this.dashboard = [
             { cols: 1, rows: 1, y: 0, x: 0 },
-            { cols: 1, rows: 1, y: 0, x: 1 }
+            { cols: 2, rows: 1, y: 1, x: 0 }
         ]
     }
 }
--- frontend/src/app/features/dashboard/dashboard.module.ts
+++ frontend/src/app/features/dashboard/dashboard.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { GridsterModule } from 'angular-gridster2'
+import { ChartsModule } from 'ng2-charts'
 
 import { SharedModule } from 'src/app/shared/shared.module'
 
@@ -8,6 +9,6 @@ import { DashboardComponent } from './components/dashboard/dashboard.component'
 
 @NgModule({
     declarations: [DashboardComponent],
-    imports: [CommonModule, SharedModule, GridsterModule]
+    imports: [CommonModule, SharedModule, GridsterModule, ChartsModule]
 })
 export class DashboardModule {}
