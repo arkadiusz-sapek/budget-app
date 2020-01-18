2b92f3a12680778372ac9748b150f1e054b9696a
Arkadiusz <arkadiusz.sapek@skygate.io>
2020-01-18 20:27:46 +0100

    Remove debug middleware

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,14 +1,13 @@
-import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 
-import { UsersModule } from '../users/users.module';
+import { UsersModule } from 'src/users/users.module';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 import { jwtConstants } from './constants';
-import { FrontendMiddleware } from './middleware';
 
 @Module({
   imports: [
@@ -23,11 +22,4 @@ import { FrontendMiddleware } from './middleware';
   providers: [AuthService, JwtStrategy],
   exports: [AuthService],
 })
-export class AuthModule {
-  //   configure(frontEnd: MiddlewareConsumer) {
-  //     frontEnd.apply(FrontendMiddleware).forRoutes({
-  //       path: '/**', // For all routes
-  //       method: RequestMethod.ALL, // For all methods
-  //     });
-  //   }
-}
+export class AuthModule {}
