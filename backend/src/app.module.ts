b775b29ea6bfbbd2da3c40ea63ff224dc9346358
Arkadiusz <arkadiusz.sapek@skygate.io>
2020-01-20 22:50:14 +0100

    Add husky to project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,27 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { UsersModule } from './users/users.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-  imports: [
-    AuthModule,
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      url: process.env.DATABASE_URL,
-      port: 5432,
-      username: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        AuthModule,
+        UsersModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: process.env.DB_HOST,
+            url: process.env.DATABASE_URL,
+            port: 5432,
+            username: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_DATABASE_NAME,
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
