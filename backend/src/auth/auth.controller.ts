f8ae9ad098bfee636d73c1bb17b321c60ce60106
Arkadiusz Sapek <sapek.arkadiusz@gmail.com>
2020-02-01 16:56:46 +0100

    test

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,34 +1,27 @@
-import {
-  Controller,
-  Get,
-  Request,
-  Post,
-  UseGuards,
-  Body,
-} from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Request, Post, UseGuards, Body } from '@nestjs/common'
+import { AuthGuard } from '@nestjs/passport'
+console.log('test')
 
-import { CredentialsDto } from 'src/users/dto/credentials.dto';
+import { CredentialsDto } from 'src/users/dto/credentials.dto'
 
-import { AuthService } from './auth.service';
+import { AuthService } from './auth.service'
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) {}
 
-  @Post('log-in')
-  async login(@Request() req) {
-    return this.authService.login(req.user);
-  }
+    @Post('log-in')
+    async login(@Request() req) {
+        return this.authService.login(req.user)
+    }
+    @Post('sign-in')
+    async register(@Body() credentials: CredentialsDto) {
+        return this.authService.register(credentials)
+    }
 
-  @Post('sign-in')
-  async register(@Body() credentials: CredentialsDto) {
-    return this.authService.register(credentials);
-  }
-
-  @UseGuards(AuthGuard('jwt'))
-  @Get('me')
-  getProfile(@Request() req) {
-    return req.user;
-  }
+    @UseGuards(AuthGuard('jwt'))
+    @Get('me')
+    getProfile(@Request() req) {
+        return req.user
+    }
 }
