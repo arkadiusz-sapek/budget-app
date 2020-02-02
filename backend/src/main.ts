3f759455ef8b91775cf5c391a5fb81f38d1ff345
Arkadiusz Sapek <sapek.arkadiusz@gmail.com>
2020-02-02 14:25:17 +0100

    Add swagger to project

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,22 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-  await app.listen(process.env.PORT || 4000);
+    const app = await NestFactory.create(AppModule)
+    app.setGlobalPrefix('api')
+
+    const options = new DocumentBuilder()
+        .setTitle('Budget-app')
+        .setDescription('The budget-app API description')
+        .setVersion('1.0')
+        .addTag('budget-app')
+        .build()
+    const document = SwaggerModule.createDocument(app, options)
+    SwaggerModule.setup('api', app, document)
+
+    await app.listen(process.env.PORT || 4000)
 }
 
-setTimeout(async () => await bootstrap(), 5000);
+setTimeout(async () => await bootstrap(), 5000)
